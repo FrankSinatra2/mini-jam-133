a0a2da124f6b54cd795ad0fa0f70bb59b7566664
mrezut <jmichaelrzeszut@gmail.com>
2023-05-27 15:16:23 -0400

    complete timer

--- src/game/Timer.ts
+++ src/game/Timer.ts
@@ -6,15 +6,9 @@ export default class Timer extends Phaser.GameObjects.Container {
 
     startTime = new Date().getTime();
 
-    // endTime = new Date().getTime();
-
-    // timer = this.endTime - this.startTime;
-
-    
 
     get time(): string {
-        // const seconds = this.timer % 60;
-        // const minutes = this.timer / 60;
+
         let elapsed = (new Date().getTime() - this.startTime) / 1000;
         let minute: string|number = Math.floor(elapsed / 60);
         let second: string|number = Math.floor(elapsed % 60);
@@ -25,7 +19,6 @@ export default class Timer extends Phaser.GameObjects.Container {
             second = '0' + second;
         }
         return minute + ':' + second
-        //return ("%02d:%02d" % [minute, second]);
     }
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
@@ -40,13 +33,6 @@ export default class Timer extends Phaser.GameObjects.Container {
         this.time;
         this,this.timerLabel.text = `Time: ${this.time}`;
     }
-
-    // update(t: number, dt: number): void {
-    //     // dt is given in ms
-    //     this.timer += dt * 1000;
-
-    //     this.timerLabel.text = `Time: ${this.time}`;
-    // }
 
     reset(): void {}
 }
