1bf40e19e38cdd96f0a3ef5f8bf4af934777518e
mrezut <jmichaelrzeszut@gmail.com>
2023-05-27 14:22:09 -0400

    almost have timer

--- src/game/Timer.ts
+++ src/game/Timer.ts
@@ -3,14 +3,23 @@ import Phaser from "phaser";
 export default class Timer extends Phaser.GameObjects.Container {
     
     private timerLabel!: Phaser.GameObjects.Text;
-    private timer = 0;
+
+    startTime = new Date().getTime();
+
+    // endTime = new Date().getTime();
+
+    // timer = this.endTime - this.startTime;
+
     
 
     get time(): string {
-        const seconds = this.timer % 60;
-        const minutes = this.timer / 60;
-
-        return `${minutes}:${seconds}`;
+        // const seconds = this.timer % 60;
+        // const minutes = this.timer / 60;
+        let elapsed = (new Date().getTime() - this.startTime) / 1000;
+        const minute = Math.floor(elapsed / 60);
+        const second = Math.floor(elapsed % 60);
+        // return `${minutes}:${seconds}`;
+        return minute + ':' + second;
     }
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
@@ -20,14 +29,21 @@ export default class Timer extends Phaser.GameObjects.Container {
         this.timerLabel = scene.add.text(x, y, `Time: ${this.time}`);
     }
 
-
-    update(t: number, dt: number): void {
-        // dt is given in ms
-        this.timer += dt * 1000;
-
-        this.timerLabel.text = `Time: ${this.time}`;
+    update(...args: any[]): void {
+            
+        this.time;
+        this,this.timerLabel.text = `Time: ${this.time}`;
     }
+
+    // update(t: number, dt: number): void {
+    //     // dt is given in ms
+    //     this.timer += dt * 1000;
+
+    //     this.timerLabel.text = `Time: ${this.time}`;
+    // }
 
     reset(): void {}
 }
+
+
 
